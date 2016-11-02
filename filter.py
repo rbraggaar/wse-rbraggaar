07a686b37e0fc05cedc67c06a386271eba8dfad7
Rob Braggaar <r.c.braggaar@outlook.com>
2016-11-02 21:10:09 +0100

    changed timing

--- filter.py
+++ filter.py
@@ -10,7 +10,6 @@ try:
     from tweepy import Stream
     from tweepy.streaming import StreamListener
     import time
-    import sys
 except:
     logging.critical("Error importing modules")
 
@@ -27,10 +26,18 @@ access_token_secret = "GIQETfqjbVRg9Az8wsuerF86Ef4UJReK5wp6UgoQsGvZH"
 class status_listener(StreamListener):
     # counter for number of tweets sent from Schiphol
     from_schiphol = 0
+    def __init__(self, time_limit=60*1):
+        self.start_time = time.time()
+        self.limit = time_limit
+
     def on_data(self, data):
         print data
         if "4.73, 52.29, 4.77, 52.32" in data:
             status_listener.from_schiphol += 1
+        if (time.time() - self.start_time) < self.limit:
+            print "end time", time.time()
+            print l.from_schiphol
+            return False
         return True
 
     def on_error(self, status_code):
@@ -40,15 +47,11 @@ class status_listener(StreamListener):
 if __name__ == '__main__':
     t_end = time.time() + 60 * 1 #1 min
     print "start time", time.time()
-    while time.time() < t_end:
-        auth = OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    l = status_listener()
+    stream = Stream(auth, l)
+    # Filters the Twitter stream to capture data by location Amsterdam
+    stream.filter(locations=[4.73,52.29,4.98,52.42])
 
-        l = status_listener()
-        stream = Stream(auth, l)
 
-        # Filters the Twitter stream to capture data by location Amsterdam
-        stream.filter(locations=[4.73,52.29,4.98,52.42])
-    print "end time", time.time()
-    print l.from_schiphol
-    sys.exit()
